daedb9eb44d59e3916170b70c4a415f4ccbf7674
seankooo <rinnae152@gmail.com>
2024-03-07 03:10:40 -0700

    created a pdf file viewer on project page. The viewer's width is set to its parent's width, so it is too narrow. I will edit this width to a full page pdf viewer later.

--- index.js
+++ index.js
@@ -36,3 +36,26 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+
+function togglePDF(pdfFilePath, pdfContainer, pdfEmbed) {
+  var pdfContainer = document.getElementById(pdfContainer);
+  var pdfEmbed = document.getElementById(pdfEmbed);
+
+  if (pdfContainer.classList.contains("project-details__documents-hidden")) {
+      // Show PDF container
+      pdfContainer.classList.remove("project-details__documents-hidden");
+      pdfContainer.classList.add("project-details__documents-fullWidth");
+      pdfContainer.style.height = '600px'; // Set height to accommodate PDF
+      pdfContainer.classList.add("project-details__documents-fullWidth");
+      pdfEmbed.setAttribute("src", pdfFilePath);
+
+  } else {
+      // Hide PDF container
+      pdfContainer.classList.remove("project-details__documents-fullWidth");
+      pdfContainer.classList.add("project-details__documents-hidden");
+      pdfContainer.style.height = "0"; // Set height to 0 to hide PDF
+      pdfEmbed.setAttribute("src", ""); // Clear the PDF source
+  }
+}
+
